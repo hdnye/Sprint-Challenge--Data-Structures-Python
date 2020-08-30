c9ceffe7a9203a16cae570a90f630d60a01f283e
Heather Nye <heatherdnye@gmail.com>
2020-08-30 18:44:50 -0500

    first try ringbuffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,17 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+       self.capacity = capacity
+       self.data = []
+       self.cur_node = 0
 
     def append(self, item):
-        pass
+        if self.data == self.capacity:
+            self.cur_node = 0
+        self.data[self.cur_node] = item
+        self.cur_node = [self.cur_node+1] % self.capacity
+        return self.data
 
     def get(self):
-        pass
+        return self.data     
+
+    
