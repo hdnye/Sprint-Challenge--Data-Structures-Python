407c34f532495a5e2d1543d5871a6f42cb5f8b8f
Heather Nye <heatherdnye@gmail.com>
2020-08-30 19:10:34 -0500

    ring_buffer passing tests

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -2,14 +2,19 @@ class RingBuffer:
     def __init__(self, capacity):
        self.capacity = capacity
        self.data = []
-       self.cur_node = 0
+       self.size = 0
 
     def append(self, item):
         if self.data == self.capacity:
-            self.cur_node = 0
-        self.data[self.cur_node] = item
-        self.cur_node = [self.cur_node+1] % self.capacity
-        return self.data
+            return None
+        if len(self.data) == self.capacity:
+               self.data[self.size] = item
+               if self.size + 1 == self.capacity:
+                   self.size = 0
+               else: 
+                   self.size += 1
+        else: 
+            self.data.append(item)
 
     def get(self):
         return self.data     
